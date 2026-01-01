984d2fb8d7079b5e0a7311ec3d0b915c18762b5a
Rodney Mullins <ROD2171612@maricopa.edu>
2026-01-01 18:11:15 +0000

    Fixed GEnai version

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      // Polyfill process.env.API_KEY for the SDK
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    }
   }
 })
